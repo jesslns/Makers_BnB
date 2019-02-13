0ef057b58e8118d9414a3a32fdaa26bb050acbba
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-13 11:41:56 +0000

    refactored unit tests to test owner foreign key

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,16 +1,23 @@
 require_relative '../app/lib/space.rb'
+require_relative '../app/lib/user.rb'
 require 'database_helper'
 
 describe Space do
   before(:each) do
     database_wiper
+    User.create(
+      username: "MakersPerson",
+      email:"Awesome@aol.com",
+      pass_hash: "DoesntKnow"
+    )
   end
 
   it 'can create a space object' do
     space = Space.new(
       space_name: "Makers",
       description:"Awesome",
-      price: 25.00
+      price: 25.00,
+      owner_id: User.find_by(username: "MakersPerson")
     )
     expect(space).to be_kind_of(Space)
   end
@@ -19,8 +26,22 @@ describe Space do
     new_table_row = Space.create(
       space_name: "Makers",
       description:"Awesome",
-      price: 25.00
+      price: 25.00,
+      owner_id: User.find_by(username: "MakersPerson").id
     )
     expect(Space.all).to include(new_table_row)
+  end
+
+  it 'throws an error if trying to create a space with a non-existant
+  owner_id' do
+    expect{
+      new_table_row = Space.create(
+        space_name: "Makers",
+        description:"Awesome",
+        price: 25.00,
+        owner_id: 9999
+        #test may not work on brand new testing database
+      )
+    }.to raise_error()
   end
 end

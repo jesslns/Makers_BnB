6bf3ce365db5ec9ce4d1b9db92a457c4c9924786
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-15 12:00:15 +0000

    add Booking.available? to Booking

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -4,7 +4,8 @@ require_relative '../app/lib/booking.rb'
 require 'database_helper'
 
 describe Booking do
-before(:each) do
+
+  before(:each) do
   User.create(
     username: "MakersPerson",
     email:"Awesome@aol.com",
@@ -16,14 +17,15 @@ before(:each) do
     price: 25.00,
     owner_id: User.find_by(username: "MakersPerson")
   )
-end
+  end
+
   it 'can make a booking'do
   booking = Booking.new(
     user_id: User.find_by(username: "MakersPerson").id,
     space_id: Space.find_by(space_name: "Makers").id,
     booking_date: Time.parse("14/02/2019")
   )
-end
+  end
 
   it 'can persist a booking object' do
     new_table_row = Booking.create(
@@ -32,6 +34,18 @@ end
       booking_date: Time.parse("14/02/2019")
     )
     expect(Booking.all).to include(new_table_row)
-end
+  end
+
+  it 'checks if a booking exists and returns true or false' do
+    Booking.create(
+      user_id: User.find_by(username: "MakersPerson").id,
+      space_id: Space.find_by(space_name: "Makers").id,
+      booking_date: Time.parse("14/02/2019")
+    )
+    date = Time.parse("14/02/2019")
+    date1 = Time.parse("14/03/2019")
+    expect(Booking.available?(Space.find_by(space_name: "Makers").id, date)).to eq(false)
+    expect(Booking.available?(Space.find_by(space_name: "Makers").id, date1)).to eq(true)
+  end
 
 end
